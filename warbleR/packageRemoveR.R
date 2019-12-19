d46fbc10456ae52de8a2e2e59bb723b5fb5b9b47
Guillaume Gaudin <34863251+g-gaudin@users.noreply.github.com>
2019-12-19 17:45:55 +0100

    Delete packageRemoveR.R

--- warbleR/packageRemoveR.R
+++
@@ -1,8 +0,0 @@
-# Title     : packageRemoveR
-# Objective : Remove packages
-# Created by: Valentin Zelionii
-# Created on: 17.12.2019
-
-remove.packages("warbleR")
-remove.packages("Rraven")
-remove.packages("devtools")
